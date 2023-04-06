d469f4ea6248f6626dd82a73c57faffcce241bfa
Manas12Singh <89171896+Manas12Singh@users.noreply.github.com>
2023-04-06 15:27:20 +0530

    wh

--- man25.c
+++ man25.c
@@ -1,4 +1,5 @@
 //wacp to demonstrate the use of realloc() function. 
+//Home work//
 //wacp to accept n integers from the usr and find the xor of all the array elements. implement it using dma.
 //wacp to check whether the given two strings are anagrams of each other or not.
 #include <stdio.h>
